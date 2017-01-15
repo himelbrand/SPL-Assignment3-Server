1c3797c7cc76c8b5bc2180bed5879c03208df33b
Shahar <nussbaus@post.bgu.ac.il>
2017-01-15 19:53:17 +0200

    hello man גשדשדג

--- src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
+++ src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -1,10 +1,14 @@
 package bgu.spl171.net.impl.TFTPtpc;
 
+import bgu.spl171.net.api.MessageEncoderDecoder;
+import bgu.spl171.net.api.bidi.BidiMessagingProtocol;
 import bgu.spl171.net.impl.newsfeed.NewsFeed;
 import bgu.spl171.net.impl.rci.ObjectEncoderDecoder;
 import bgu.spl171.net.impl.rci.RemoteCommandInvocationProtocol;
+import bgu.spl171.net.srv.BidiMessagingProtocolImpl;
+import bgu.spl171.net.srv.MessageEncoderDecoderImpl;
 import bgu.spl171.net.srv.Server;
-
+import bgu.spl171.net.srv.msg.Message;
 
 
 /**
@@ -14,13 +18,12 @@ public class TPCMain {
 
     public static void main(String[] args) {
 
-//
-//        Server.threadPerClient(
-//                Runtime.getRuntime().availableProcessors(),
-//                7777, //port
-//                () ->  new RemoteCommandInvocationProtocol<>(feed), //protocol factory
-//                ObjectEncoderDecoder::new //message encoder decoder factory
-//        ).serve();
+
+        Server.threadPerClient(
+                Integer.parseInt(args[0]), //port
+                BidiMessagingProtocolImpl::new, //protocol factory
+                MessageEncoderDecoderImpl::new //message encoder decoder factory
+        ).serve();
 
     }
 }
